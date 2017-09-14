dab74149c47dc8fb325ae63865f025483070b212
Arcaa <31939186+Arcaa@users.noreply.github.com>
2017-09-14 10:20:28 +0200

    Delete Pe2_4.py

--- Programming/Pe2_4.py
+++
@@ -1,4 +0,0 @@
-uurloon=float(input('Hoeveel verdien je per uur ? '))
-werk=float(input('Hoeveel uur heb je gewerkt ? '))
-totaal=uurloon*werk
-print(str(werk)+' uur werken levert ' + str(totaal) + ' Euro op')
